be14dc18ff9ee8d7c2deea6d199fd97f210364a5
Haoyu Li <aide0740@gmail.com>
2022-02-13 22:55:46 -0500

    stage changes

--- Spline.py
+++ Spline.py
@@ -1,12 +1,132 @@
 from scipy.interpolate import splprep, splev
 from scipy.interpolate import BSpline
+import numpy as np
+import time
+import matplotlib.pyplot as plt
 
 class AKBSpline:
-    def __init__(self, curve_sequence) -> None:
+    def __init__(self, curve_sequence:np.array, num_knots:np.array = 10, degree:int = 3) -> None:
+        assert curve_sequence.shape[0] >= degree + 2
+        assert num_knots >= (degree + 1) * 2
+        # self.curve_sequence = self._normalize(curve_sequence,True)
         self.curve_sequence = curve_sequence
+        self.u = self._get_u(curve_sequence)
+        p = degree + 1
+        self.p = p
+        self.num_knots = num_knots
+        self.r = self.num_knots - 2 * degree
+        # calculate p-th derivative of the input curve
+        deri = []
+        for pp in range(p):
+            if pp == 0:
+                next_deri = self._next_derivative(self.curve_sequence,self.u)
+            else:
+                next_deri = self._next_derivative(deri[pp-1][0], deri[pp-1][1])
+            deri.append(next_deri)
+        self.f, self.u_f = self._get_feature_function(deri[-1][0],deri[-1][1])
+        self.F = self._integral_f()
+        u_array = self._cal_auto_knots()
+        self.knots = np.zeros((self.num_knots))
+        self.knots[degree:-degree] = u_array
+        self.knots[-degree:] = 1
+        tcku, fp, ier, msg = splprep(self.curve_sequence.T, t=self.knots, k = degree, task=-1, full_output=True, quiet=True)
+        
+        self.tcku = tcku
+        self.fp = fp
 
-    def _cal_auto_knots():
-        pass
+    def _normalize(self, seq:np.array, keep_aspect = False) -> np.array:
+        if not keep_aspect:
+            min_seq = np.min(seq, axis = 0, keepdims=True)
+            max_seq = np.max(seq, axis = 0, keepdims=True)
+        else: 
+            min_seq = np.min(seq)
+            max_seq = np.max(seq)
+        seq = (seq - min_seq) / (max_seq - min_seq)
+        return seq
 
-    def _next_derivative():
-        pass
+    def _get_u(self, coords:np.array):
+        dist = np.sqrt(np.sum((coords[1:] - coords[:-1]) ** 2,axis=1))
+        acc_dist = np.zeros((coords.shape[0],))
+        acc_dist[1:] = np.add.accumulate(dist)
+        return acc_dist/ dist.sum()
+
+    def _get_feature_function(self, p_th_deri:np.array, u:np.array):
+        f = np.zeros((len(u) + 2))
+        u_f = np.zeros_like(f)
+        u_f[1:-1] = u
+        u_f[-1] = 1
+        f[1:-1] = np.linalg.norm(p_th_deri,axis=1) ** (1/(self.p))
+        return f, u_f
+
+    def _integral_f(self):
+        f = self.f 
+        u_f = self.u_f
+        eps = 1e-10
+        F = np.zeros_like(f)
+        f_trap = np.zeros_like(f)
+        acc = 0
+        f_trap[1:] = 0.5 * (f[:-1] + f[1:] + eps) * (u_f[1:] - u_f[:-1])
+        acc = f_trap.sum()
+        del_f = acc / (self.r-1)
+        acc = 0
+        f_trap_del = np.where(f_trap<del_f,f_trap,del_f)
+        F = np.add.accumulate(f_trap_del)
+        return F
+
+    def _cal_auto_knots(self):
+        F = self.F
+        u_f = self.u_f
+        F_array = np.linspace(0,F[-1],self.r )
+        idx = 0
+        i = 0
+        u_array = np.zeros_like(F_array)
+        while True:
+            interval = F[idx+1] - F[idx]
+            if F_array[i] > F[idx+1]:
+                idx += 1
+                continue
+            else:
+                alpha = (F_array[i] - F[idx]) / interval
+                u_array[i] = (1-alpha) * u_f[idx] + alpha * u_f[idx+1]
+            i += 1
+            if i == len(F_array):
+                break
+        return u_array
+
+    def _next_derivative(self, sequence, u):
+        xyz_diff = sequence[1:] - sequence[:-1]
+        u_diff = u[1:] - u[:-1]
+        new_u = (u[:-1] + u[1:]) / 2
+        deri = xyz_diff / u_diff[:,None]
+        return [deri, new_u]
+
+
+    def _dist(self, coord1:np.array, coord2:np.array):
+        return np.sqrt(np.sum((coord1 - coord2) ** 2))
+
+if __name__ == "__main__":
+    l = 30
+    k = 3
+    n_knots = 50
+    tra = np.load('trajectories.npy')
+    min_coord = np.array([   0. ,        0.,         0. ,     -999.64136, -999.7137,  -997.5717 ], dtype=np.float32)[None,None,:]
+    max_coord = np.array([6.9077554e+00, 9.9213535e-01, 6.2831855e+00, 9.9986377e+02, 9.9980994e+02, 9.9779254e+02], dtype=np.float32)[None,None,:]
+    #normalization
+    tra = (tra - min_coord) / (max_coord - min_coord)
+    length = np.load('tra_len.npy')
+    trace = tra[0,:length[0],3:]
+    spline = AKBSpline(trace,n_knots,k)
+    tck, u = spline.tcku
+    print(spline.knots)
+    print(spline.F)
+    new_points = splev(u, tck)
+    
+    fig = plt.figure(figsize=(18, 10), dpi=80)
+    ax = fig.add_subplot(1,2,1,projection='3d')
+    ax2 = fig.add_subplot(1,2,2,projection='3d')
+    # ax.scatter(new_points[0][:l], new_points[1][:l], new_points[2][:l], '.',c=rgb_cycle)
+    ax.plot(new_points[0][:l],new_points[1][:l],new_points[2][:l])
+
+    # ax3.scatter(trace[:l,0], trace[:l,1], trace[:l,2], 'o',c=rgb_cycle)
+    ax2.plot(trace[:l,0], trace[:l,1], trace[:l,2])
+    plt.show()
--- train_spline.py
+++ train_spline.py
@@ -11,77 +11,113 @@ import numpy as np
 class Spline(Dataset):
     def __init__(self, fname):
         self.spline = np.load(fname)
+        self.n, self.t, self.d = self.spline.shape
+        self.t = self.t - 1
+        self.start_min = self.spline[:,0,:].min()
+        self.start_max = self.spline[:,0,:].max()
+        # self.start_mean = self.spline[:,0,:].mean()
+        self.spline[:,0,:] = (self.spline[:,0,:] - self.start_min) / (self.start_max - self.start_min)
+        # from matplotlib import pyplot as plt 
+        # plt.hist(self.spline[:,0,:].flatten())
+        # plt.show()
+        # exit()
 
     def __getitem__(self, index):
-        return self.spline[index][0],self.spline[index][1:]
+        tra_idx = index // self.t
+        t_idx = index - (self.t * tra_idx)
+        start = self.spline[tra_idx][0] 
+        time = np.array([t_idx / self.t],dtype=np.float32)
+        end = self.spline[tra_idx][t_idx+1]
+        return start, time, end
     def __len__(self):
-        return len(self.spline)
+        return self.n * self.t
+
+class Basic(nn.Module):
+    def __init__(self, cin, cout):
+        super().__init__()
+        self.basic = nn.Sequential(
+            nn.Linear(cin,cout),
+            nn.LayerNorm(cout),
+            nn.ReLU(),
+        )
+    def forward(self, x):
+        return self.basic(x)
 
 class Net(nn.Module):
-    def __init__(self,latent_size,num_control_point):
+    def __init__(self):
         super(Net, self).__init__()
-        self.mlp1 = nn.Linear(num_control_point * 3,512)
-        self.mlp2 = nn.Linear(512,256)
-        self.mlp3 = nn.Linear(256,latent_size)
-        self.fc1 = nn.Linear(latent_size, 256)
-        self.fc2 = nn.Linear(256, 512)
-        self.fc3 = nn.Linear(512, num_control_point * 3)
+        self.start_mlp = nn.Sequential(
+            Basic(3,64),
+            Basic(64,128),
+            Basic(128,256),
+            Basic(256,512),
+            nn.Linear(512,512),
+            nn.ReLU(),
+        )
+        self.time_mlp = nn.Sequential(
+            Basic(1,16),
+            Basic(16,32),
+            Basic(32,64),
+            Basic(64,128),
+            Basic(128,256),
+            Basic(256,512),
+            nn.Linear(512,512),
+            nn.ReLU(),
+        )
+        self.end_mlp = nn.Sequential(
+            Basic(1024,1024),
+            Basic(1024,512),
+            Basic(512,256),
+            Basic(256,128),
+            Basic(128,64),
+            nn.Linear(64,3),
+            nn.Sigmoid(),
+        )
 
-    def encoder(self, x):
-        x = self.mlp1(x)
-        x = F.relu(x)
-        x = self.mlp2(x)
-        x = F.relu(x)
-        x = self.mlp3(x)
-        return x
-
-    def decoder(self, x):
-        x = F.relu(x)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        x = F.relu(x)
-        x = self.fc3(x)
-        y = F.sigmoid(x)
-        return y
-
-    def forward(self,x):
-        z = self.encoder(x)
-        y = self.decoder(z)
+    def forward(self,start, time):
+        z1 = self.start_mlp(start)
+        z2 = self.time_mlp(time)
+        z = torch.cat([z1,z2],dim=-1)
+        y = self.end_mlp(z)
         return y
 
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device).view(-1,args.num_control_points * 3)
+    for batch_idx, (start, time, end) in enumerate(train_loader):
+        start, time, end = start.to(device), time.to(device), end.to(device)
+        # print(start[0],time[0])
         optimizer.zero_grad()
-        output = model(target)
-        loss = F.mse_loss(output, target, reduction='mean')
+        output = model(start, time)
+        # print(output[0])
+        # print(end[0])
+        loss = F.mse_loss(output, end, reduction='mean')
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
+                epoch, batch_idx * len(start), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
             if args.dry_run:
                 break
 
 
-def test(args, model, device, test_loader):
+def test(model, device, test_loader):
     model.eval()
     test_loss = 0
-    all_output = np.zeros((len(test_loader.dataset),args.num_control_points,3),dtype=np.float32)
-    idx = 0
+    # n = test_loader.dataset.n
+    # t = test_loader.dataset.t
+    # all_output = np.zeros([n * t, 3],dtype=np.float32)
+    # idx = 0
     with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device).view(-1,args.num_control_points * 3)
-            output = model(target)
-            test_loss += F.mse_loss(output, target, reduction='mean').item() * len(data)  # sum up batch loss
-            all_output[idx:idx+len(data)] = output.view(-1,args.num_control_points,3).cpu().numpy()
-            idx += len(data)
-    np.save('output_spline',all_output)
+        for start, time, end in test_loader:
+            start, time, end = start.to(device), time.to(device), end.to(device)
+            output = model(start, time)
+            test_loss += F.mse_loss(output, end, reduction='mean').item() * len(start)  # sum up batch loss
+            # all_output[idx:idx + len(start)] = output.cpu().numpy()
+            # idx += len(start)
+    # np.save('output_spline',all_output)
 
     test_loss /= len(test_loader.dataset)
 
@@ -92,9 +128,9 @@ def test(args, model, device, test_loader):
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=10240, metavar='N',
                         help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+    parser.add_argument('--test-batch-size', type=int, default=30000, metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=14, metavar='N',
                         help='number of epochs to train (default: 14)')
@@ -112,10 +148,6 @@ def main():
                         help='how many batches to wait before logging training status')
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
-    parser.add_argument('--latent-size', type=int, default=20, metavar='N',
-                        help='Latent dimensionality')
-    parser.add_argument('--num-control-points', type=int, default=96, metavar='N',
-                        help='Number of control points')
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -132,18 +164,18 @@ def main():
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
-    dataset = Spline('spline.npy')
+    dataset = Spline('spline_training.npy')
     train_loader = torch.utils.data.DataLoader(dataset,**train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset, **test_kwargs)
 
-    model = Net(args.latent_size,args.num_control_points).to(device)
+    model = Net().to(device)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     last_loss = -1
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
-        test_loss = test(args, model, device, test_loader)
+        test_loss = test(model, device, test_loader)
         if last_loss != -1:
             if  abs(last_loss - test_loss) < 1e-8:
                 break
@@ -151,7 +183,7 @@ def main():
         scheduler.step()
 
     if args.save_model:
-        torch.save(model.state_dict(), "spline.pth")
+        torch.save(model.state_dict(), "spline_model.pth")
 
 
 if __name__ == '__main__':
